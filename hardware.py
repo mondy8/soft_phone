59e464f0bf056db3562a599bca7a8a25cac330f5
今岡 宏朗 <wmoaiw@gmail.com>
2019-03-03 15:27:52 +0900

    hardware.py更新

--- hardware.py
+++ hardware.py
@@ -2,6 +2,7 @@ import time
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8, GPIO.OUT)
 GPIO.setup(18, GPIO.IN)
 
 while(1):
@@ -9,6 +10,12 @@ while(1):
     # GPIO18ピンの入力状態を表示する
     print("input:"+str(value))
     time.sleep(0.1)
+
+    if value == 0 :
+        GPIO.output(8, 0)
+    elif value == 1 :
+        GPIO.output(8, 1)
+
 
 # GPIOピンをリセット
 # GPIO.cleanup()
