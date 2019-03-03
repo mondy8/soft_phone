5de4c21f22449333b7232f510fa01c9cc8b7d8ef
今岡 宏朗 <wmoaiw@gmail.com>
2019-03-03 15:15:10 +0900

    hardware.py更新

---
+++ a.py
@@ -0,0 +1,11 @@
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(18, GPIO.IN)
+
+try: 
+    while(1):
+        value = GPIO.input(18)
+        # GPIO18ピンの入力状態を表示する
+        # print("input:"+str(value))
--- hardware.py
+++ hardware.py
@@ -4,11 +4,10 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(18, GPIO.IN)
 
-try: 
-    while(1):
-        value = GPIO.input(18)
-        # GPIO18ピンの入力状態を表示する
-        print("input:"+str(value))
+while(1):
+    value = GPIO.input(18)
+    # GPIO18ピンの入力状態を表示する
+    print("input:"+str(value))
 
 # GPIOピンをリセット
 # GPIO.cleanup()
