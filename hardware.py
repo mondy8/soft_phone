0613cffd08ccd3637d0b293b2045b31633043b40
今岡 宏朗 <wmoaiw@gmail.com>
2019-03-03 12:53:53 +0900

    hardware.py更新

--- hardware.py
+++ hardware.py
@@ -3,9 +3,10 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN)
- 
-# GPIO18ピンの入力状態を表示する
-print GPIO.input(18)
- 
-# GPIOピンをリセット
-GPIO.cleanup()
+
+
+while(1):
+    # GPIO18ピンの入力状態を表示する
+    print (GPIO.input(18))
+    # GPIOピンをリセット
+    GPIO.cleanup()
