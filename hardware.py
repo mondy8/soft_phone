4e12eeb9a2864d1ae1371d64089c3ff509f92131
今岡 宏朗 <wmoaiw@gmail.com>
2019-03-03 15:58:50 +0900

    hardware.py更新

--- hardware.py
+++ hardware.py
@@ -1,20 +1,33 @@
 import time
 import RPi.GPIO as GPIO
 
+valve = 8
+motor_inp1 = 10 #motor input1 右回り
+motor_inp2 = 12 #motor input1 左回り
+pressure = 18 #圧力センサ
+
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(8, GPIO.OUT)
-GPIO.setup(18, GPIO.IN)
+GPIO.setup(valve, GPIO.OUT)
+GPIO.setup(motor_inp1, GPIO.OUT)
+GPIO.setup(motor_inp2, GPIO.OUT)
+GPIO.setup(pressure, GPIO.IN)
 
 while(1):
-    value = GPIO.input(18)
+    ####圧力確認
+    value = GPIO.input(pressure)
     # GPIO18ピンの入力状態を表示する
     print("input:"+str(value))
     time.sleep(0.1)
 
+    ####モーター駆動
+    GPIO.output(motor_inp1, 1)
+    GPIO.output(motor_inp2, 0)
+
+    ####valve
     if value == 0 :
-        GPIO.output(8, 0)
+        GPIO.output(valve, 0)
     elif value == 1 :
-        GPIO.output(8, 1)
+        GPIO.output(valve, 1)
 
 
 # GPIOピンをリセット
